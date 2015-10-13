ed945dc60bb05cdeaa10667fa8fee074abf278b2
Radu <radu.bordei89@gmail.com>
2015-10-13 17:39:23 +0300

    improve comments trait.

--- src/CommentTrait.php
+++ src/CommentTrait.php
@@ -2,6 +2,8 @@
 
 namespace Eloquent\Commentable;
 
+use Illuminate\Database\Eloquent\Model;
+
 trait CommentTrait {
 
     /**
@@ -10,4 +12,34 @@ trait CommentTrait {
     public function comments() {
         return $this->morphMany(Comment::class, 'commentable');
     }
+
+    /**
+     *  Add comment ..
+     *
+     * @param $comment
+     * @param Model $author
+     * @param string $status
+     */
+    public function addComment($comment, Model $author, $status = 'pending') {
+        $commentRow = new Comment([
+            'author_id' => $author->id,
+            'author_type' => $author->getMorphClass(),
+            'comment' => $comment,
+            'status' => $status,
+        ]);
+
+        return $this->comments()->save($commentRow);
+    }
+
+    /**
+     * Delete comment by id .
+     *
+     * @param $comment_id
+     */
+    public function deleteComment($comment_id) {
+        return $this->comments()
+            ->where('id', $comment_id)
+            ->delete();
+    }
+
 }
